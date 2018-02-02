b0d28f3c09a9482bc4aeeaec196bd4e31aa63add
Byron Griffith <byrongriffith91@gmail.com>
2018-02-02 15:37:34 -0500

    containers added

--- client/src/pages/addchore/addchore.js
+++ client/src/pages/addchore/addchore.js
@@ -5,9 +5,17 @@ import { Col, Row, Container } from "../../components/grid";
 class addChore extends Component {
     render() {
         return (  
-            <div>
-                <h1>Add a Chore Yo</h1>
-            </div>
+            <Container fluid>
+                <form>
+                    <div class="form-group">
+                        <label for="AddAChoreHere">Add A Chore</label>
+                        <input type="addChore" class="form-control" id="AddAChore" placeholder="Add a chore by name here" />
+                    </div>
+                        <label for="Describe the Chore">Chore Description</label>
+                        <textarea class="form-control" rows="3" placeholder="Describe the chore here"></textarea>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+            </Container>
         );
     }
 }

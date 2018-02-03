caf7228e1b21a157ac49c15ccfd7477a229e5e58
Byron Griffith <byrongriffith91@gmail.com>
2018-02-03 12:43:02 -0500

    fixy McfixFace

--- client/src/pages/poolMember/poolMember.js
+++ client/src/pages/poolMember/poolMember.js
@@ -5,7 +5,31 @@ import { Col, Row, Container } from "../../components/grid";
 class poolMember extends Component {
     render() {
         return (  
-            
+            <Container fluid>
+                <br />
+                    <Row>
+                        <Col size="md-8">
+                            <div className="card text-white bg-primary mb-3">
+                                <div className="card-header">Task</div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Clean the Kitchen</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </Col>
+
+                        <Col size="md-4">
+                            <div className="card text-white bg-primary mb-3">
+                                <div className="card-header">Fill  the Pool</div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Reward:</h4>
+                                    <h5>Go to the Movies</h5>
+                                    <p className="card-text">75/100%</p>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+            </Container>
         );
     }
 }

e65886eb0340a17cd020dcfd5f71c855f79a1987
Bo Slott <30351649+boslott@users.noreply.github.com>
2018-02-03 15:34:46 -0500

    added input names for Chore form and Pool form

--- client/src/pages/createPool/createPool.js
+++ client/src/pages/createPool/createPool.js
@@ -7,12 +7,17 @@ class createPool extends Component {
         return (
             <div>
                 <form>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="AddAPoolHere">Add A Pool</label>
-                        <input type="addPool" class="form-control" id="AddAPool" placeholder="Add a Pool by name here" />
+                        <input type="text" className="form-control" id="addAPool" placeholder="Add a Pool by name here" name="name" />
                     </div>
+                    <div className="form-group">
                         <label for="Describe the Pool">Pool Description</label>
-                        <textarea class="form-control" rows="3" placeholder="Describe the Pool here"></textarea>
+                        <textarea className="form-control" rows="3" placeholder="Describe the Pool here" name="description"></textarea>
+                    <div classNamne="form-group">
+                        <label for="reward">Reward</label>
+                        <input type="text" className="form-control" id="addReward" placeholder="Add a reward here" name="reward" />
+                    </div>
                         <button type="submit" class="btn btn-default">Submit</button>
                 </form>
             </div>

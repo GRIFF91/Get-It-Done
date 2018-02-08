25a09f4a1b6a124f0829150896c9ad1f5839b9de
Byron Griffith <byrongriffith91@gmail.com>
2018-02-07 21:30:27 -0500

    poolTemplate page added

--- client/src/pages/poolManager/poolManager.js
+++ client/src/pages/poolManager/poolManager.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/grid";
+import "./poolManager.css";
 
 class poolManager extends Component {
     render() {
         return (  
-            <div>
-                <h1>Pool Manager Page</h1>
-            </div>
+            <div>  
+            <Container fluid>
+                <br />
+                    <Row>
+                        <Col size="md-12">
+                            <div className="card text-white bg-primary mb-3">
+                                <div className="card-header">Pool Name</div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Clean the Kitchen</h4>
+                                        <ul>
+                                            <li></li>
+                                            <li></li>
+                                            <li></li>
+                                            <li></li>
+                                        </ul>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+            </Container>
+        </div>
         );
     }
 }
